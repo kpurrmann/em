b9f3b9f2487c6c6c5094b066469cdeb6d04d2b2c
kevin <kevin@crusty>
2013-04-12 12:51:53 +0200

    Set up Doctrine, ACL and Backendusermanagement

--- public/index.php
+++ public/index.php
@@ -1,10 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        COOL TEST
-    </body>
-</html>
+<?php
+
+define('REQUEST_MICROTIME', microtime(true));
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
