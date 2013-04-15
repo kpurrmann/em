d89659c9025d3e1e5be90aed4d7a1da2392bbb2b
kevin <kevin@crusty>
2013-04-15 17:39:32 +0200

    Setup Testconfig

---
+++ module/Em/test/EmTest/ModuleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @link      https://github.com/weierophinney/PhlySimplePage for the canonical source repository
+ * @copyright Copyright (c) 2012 Matthew Weier O'Phinney (http://mwop.net)
+ * @license   https://github.com/weierophinney/PhlySimplePage/blog/master/LICENSE.md New BSD License
+ */
+
+namespace EmTest;
+
+use Em\Module;
+use PHPUnit_Framework_TestCase as TestCase;
+use stdClass;
+use Zend\EventManager\EventManager;
+use Zend\EventManager\SharedEventManager;
+use Zend\EventManager\StaticEventManager;
+use Zend\Mvc\Application;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+
+/**
+ * Unit tests for PhlySimplePage\Module
+ */
+class ModuleTest extends TestCase
+{
+
+    public function testTry()
+    {
+        $this->markTestIncomplete();
+    }
+
+}
