b02f2db3df6c2ca370d66c02e61eddf2ad50d303
kevin <kevin@crusty>
2013-04-25 15:27:27 +0200

    [TASK] Added Tests for EevntControllerFactoryTest

---
+++ module/Event/test/EventTest/Controller/EventControllerFactoryTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace EventTest\Controller;
+
+/**
+ * Description of EventControllerFactoryTest
+ *
+ * @author Kevin Purrmann <k.purrmann@familie-redlich.de>
+ */
+class EventControllerFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     *
+     * @var \Event\Controller\EventControllerFactory
+     */
+    protected $factory;
+
+    public function setUp()
+    {
+        $this->events = new EventManager();
+        $this->sharedEvents = new SharedEventManager();
+        $this->events->setSharedManager($this->sharedEvents);
+
+        $this->application = new TestAsset\Application();
+        $this->application->setEventManager($this->events);
+        $this->application->setServiceManager(Bootstrap::getServiceManager());
+        $this->event = new MvcEvent();
+        $this->event->setApplication($this->application);
+        $this->event->setTarget($this->application);
+        $this->event->setRouteMatch(new \Zend\Mvc\Router\Http\RouteMatch(array()));
+
+        $this->module = new \Event\Module();
+    }
+
+    public function testFactoryCreatesEventControllerInstance()
+    {
+        $this->dispatch('/');
+        $this->assertResponseStatusCode(200);
+
+//        $this->assertModuleName('Album');
+//        $this->assertControllerName('Album\Controller\Album');
+//        $this->assertControllerClass('AlbumController');
+//        $this->assertMatchedRouteName('album');
+    }
+
+}
