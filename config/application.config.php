946835f79ba2a6a6c816b5ae9a932200d909615f
kevin <kevin@crusty>
2013-06-12 11:17:26 +0200

    [TASK] Included Importer Module

--- config/application.config.php
+++ config/application.config.php
@@ -7,6 +7,7 @@ return array(
       'DoctrineModule',
       'DoctrineORMModule',
       'Event',
+      'Importer',
    ),
    // These are various options for the listeners attached to the ModuleManager
    'module_listener_options' => array(
