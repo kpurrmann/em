4b8cdfb02e73b261b9f1f88575eb4df67b5300d1
kevin <kevin@crusty>
2013-06-14 15:51:12 +0200

    [TASK] SetUp ImportController

--- module/Importer/src/Importer/Controller/ImportController.php
+++ module/Importer/src/Importer/Controller/ImportController.php
@@ -18,32 +18,60 @@ class ImportController extends AbstractActionController
 
     /**
      *
+     * @var \Importer\Service\ImportServiceInterface
+     */
+    protected $importService;
+
+    /**
+     *
      * @return \Zend\View\Model\ViewModel
      */
     public function indexAction()
     {
 
-        $excel = \PHPExcel_IOFactory::createReaderForFile('/var/www/em/data/uploads/test.xml');
-        /* @var $sheet \PHPExcel */
-        $sheet = $excel->load('/var/www/em/data/uploads/test.xml');
+        $form = $this->importService->getForm();
+        if ($id = $this->params('id')) {
 
-        /* @var $active \PHPExcel_Worksheet */
-        $active = $sheet->getActiveSheet();
+            $viewModel = new ViewModel();
+            if ($this->getRequest()->isXmlHttpRequest()) {
+                $viewModel->setTerminal(true);
+            }
 
-        /* @var $row \PHPExcel_Worksheet_Row */
-        /* @var $cell \PHPExcel_Cell */
-        foreach ($active->getRowIterator(2) as $row) {
-            foreach ($row->getCellIterator() as $cell) {
-                if (!is_float($cell->getValue())) {
-                    \Zend\Debug\Debug::dump($cell->getValue());
+            $prg = $this->fileprg($form);
+            if ($prg instanceof \Zend\Http\PhpEnvironment\Response) {
+                return $prg; // Return PRG redirect response
+            } elseif (is_array($prg)) {
+                if ($form->isValid()) {
+                    $data = $form->getData();
+                    $this->importService->parseExcel($data['file']['tmp_name'], $data['event']);
+                    unlink($data['file']['tmp_name']);
+                    return $this->redirect()->toRoute('events/action', array('action' => 'show', 'id' => $data['event']));
+                } else {
+                    $this->importService->getFlashMessenger()->addErrorMessage('Es ist ein Fehler augetreten.');
                 }
             }
-        }
-        exit;
 
-        return new ViewModel(array(
-              'events' => 'TEST'
-           ));
+            $form->get('event')->setValue($id);
+            return $viewModel->setVariables(array('form' => $form, 'id' => $id));
+        }
+    }
+
+    /**
+     *
+     * @return \Importer\Service\ImportServiceInterface
+     */
+    public function getImportService()
+    {
+        return $this->importService;
+    }
+
+    /**
+     *
+     * @param \Importer\Service\ImportServiceInterface $importService
+     */
+    public function setImportService($importService)
+    {
+        $this->importService = $importService;
     }
 
 }
