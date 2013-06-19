6f4f7d83700546a23b1ae97fc0729eb497a94ac9
kevin <kevin@crusty>
2013-06-19 10:10:33 +0200

    [TASK] AjaxAction for importing xls

--- module/Importer/src/Importer/Controller/ImportController.php
+++ module/Importer/src/Importer/Controller/ImportController.php
@@ -30,30 +30,66 @@ class ImportController extends AbstractActionController
     {
 
         $form = $this->importService->getForm();
-        if ($id = $this->params('id')) {
 
+        if ($id = $this->params('id')) {
             $viewModel = new ViewModel();
+
             if ($this->getRequest()->isXmlHttpRequest()) {
                 $viewModel->setTerminal(true);
             }
 
-            $prg = $this->fileprg($form);
-            if ($prg instanceof \Zend\Http\PhpEnvironment\Response) {
-                return $prg; // Return PRG redirect response
-            } elseif (is_array($prg)) {
+            $request = $this->getRequest();
+
+            if ($request->isPost()) {
+                $post = array_merge_recursive(
+                   $request->getPost()->toArray(), $request->getFiles()->toArray()
+                );
+
+                $form->setData($post);
+
                 if ($form->isValid()) {
-                    $data = $form->getData();
-                    $this->importService->parseExcel($data['file']['tmp_name'], $data['event']);
-                    unlink($data['file']['tmp_name']);
-                    return $this->redirect()->toRoute('events/action', array('action' => 'show', 'id' => $data['event']));
+                    if (!empty($post['isAjax'])) {
+                        $data = $form->getData();
+                        $this->importService->parseExcel($data['file']['tmp_name'], $data['event']);
+                        unlink($data['file']['tmp_name']);
+                        $model = new \Zend\View\Model\JsonModel();
+                        $model->setTerminal(true);
+                        $model->setVariables(array(
+                           'status' => true,
+                           'redirect' => $this->url()->fromRoute('events/action', array('action' => 'show', 'id' => $data['event'])),
+                           'formData' => $data,
+                        ));
+                        return $model;
+                    }
                 } else {
                     $this->importService->getFlashMessenger()->addErrorMessage('Es ist ein Fehler augetreten.');
+                    if (!empty($post['isAjax'])) {
+                        $model = new \Zend\View\Model\JsonModel();
+                        $model->setTerminal(true);
+                        return $model->setVariables(array(
+                              'status' => false,
+                              'messages' => $form->getMessages()
+                           ));
+                    }
                 }
             }
 
             $form->get('event')->setValue($id);
             return $viewModel->setVariables(array('form' => $form, 'id' => $id));
         }
+    }
+
+    /**
+     * @todo Maybe better to outsource this part cause of bootstrapping
+     * @return \Zend\View\Model\JsonModel
+     */
+    public function progressAction()
+    {
+        $model = new \Zend\View\Model\JsonModel();
+        $model->setTerminal(true);
+        $id = $this->params()->fromQuery('id', null);
+        $progress = new \Zend\ProgressBar\Upload\SessionProgress();
+        return $model->setVariables($progress->getProgress($id));
     }
 
     /**
